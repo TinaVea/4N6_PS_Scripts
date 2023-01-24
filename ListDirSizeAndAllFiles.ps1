5c9625c81d145cabe520a32ff982f740f888d57a
TinaVea <45268402+TinaVea@users.noreply.github.com>
2023-01-24 13:34:02 +0100

    Update ListDirSizeAndAllFiles.ps1

--- ListDirSizeAndAllFiles.ps1
+++ ListDirSizeAndAllFiles.ps1
@@ -1,28 +1,28 @@
-#Begin Script 
+#Begin Script
 
 #Define Input - The Directory
-$Directory = Read-Host -Prompt "Please input the directory name" 
+$Directory = (Read-Host -Prompt "Please input the directory name").Replace('"','')
 
 #Get Size of Files in the Directory
-$Files = Get-ChildItem -Path $Directory -Recurse | Select-Object -Property Name, Length 
+$Files = Get-ChildItem -Path $Directory -Recurse | Select-Object -Property Name, Length
 
 #Loop Through Files to Calculate Size in GB
 $FilesSizeInGB=@()
 foreach($File in $Files)
 {
-    $Size = [math]::Round(($File.Length/1GB),3)
-    $FileSizeInGB = [pscustomobject]@{
-        Name = $File.Name
-        SizeGB = $Size
-    } 
-    $FilesSizeInGB += $FileSizeInGB
+$Size = [math]::Round(($File.Length/1GB),3)
+$FileSizeInGB = [pscustomobject]@{
+Name = $File.Name
+SizeGB = $Size
+}
+$FilesSizeInGB += $FileSizeInGB
 }
 
-#Display Output 
+#Display Output
 $FilesSizeInGB | Format-Table -AutoSize
 
 #Ask User for Destination Directory
-$DestinationDirectory = Read-Host -Prompt "Please input the destination directory" 
+$DestinationDirectory = (Read-Host -Prompt "Please input the destination directory").Replace('"','')
 
 #Export Output to CSV
 $FilesSizeInGB | Export-Csv -Path "$DestinationDirectory\filesize.csv" -NoTypeInformation
